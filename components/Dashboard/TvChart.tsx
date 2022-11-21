8a5f52af8aed74a439790048ef8b2a5951d24e55
ImKunal13z <2002kunalmondal13@gmail.com>
2022-11-21 11:59:05 +0530

    fix: default pair added to trading view (BTC-USDC)

--- components/Dashboard/TvChart.tsx
+++ components/Dashboard/TvChart.tsx
@@ -14,10 +14,7 @@ const TvChart = () => {
     return (
         <div className="bg-transparent my-[0.75rem] m-[0.75em] min-h-[400px] col-start-1 col-end-13 rounded overflow-hidden shadow-black1">
             <AdvancedRealTimeChart
-                symbol={symbols[0] === "BTC" ? "KUCOIN:BTCUSDC" : symbols[0] === "DAI" ? "PHEMEX:DAIUSDC" : symbols[0] === "LINK" ?  "POLONIEX:LINKUSDC" : ""}
-                // PHEMEX:DAIUSDC
-                // KUCOIN:BTCUSDC
-                // POLONIEX:LINKUSDC
+                symbol={symbols[0] === "BTC" ? "KUCOIN:BTCUSDC" : symbols[0] === "DAI" ? "PHEMEX:DAIUSDC" : symbols[0] === "LINK" ?  "POLONIEX:LINKUSDC" : "KUCOIN:BTCUSDC"}
                 theme="dark"
                 autosize={true}
             />
